e8543bae5642e2b57891f9f6efb23a04794c5114
Will Moran <will.c.moran@gmail.com>
2024-05-07 13:22:30 -0400

    Added funtions and event listeners to update transient state

---
+++ scripts/transientState.js
@@ -0,0 +1,19 @@
+// Set up the transient state data structure and provide initial values
+const transientState = {
+    "ownsBlueJeans": false,
+    "socioLocationId": 0
+}
+
+
+// Functions to modify each property of transient state
+export const setOwnsBlueJeans = (chosenOwnership) => {
+    transientState.ownsBlueJeans = chosenOwnership
+    console.log(transientState)
+}
+
+export const setSocioLocationId = (chosenLocation) => {
+    transientState.socioLocationId = chosenLocation
+    console.log(transientState)
+}
+
+// Function to convert transient state to permanent state
--- scripts/ubranDweller.js
+++ scripts/ubranDweller.js
@@ -1,6 +1,17 @@
+import { setSocioLocationId } from "./transientState.js"
+
+const handleLocationChange = (changeEvent) => {
+    if (changeEvent.target.name === "location") {
+        const convertedToInteger = parseInt(changeEvent.target.value)
+        setSocioLocationId(convertedToInteger)
+    }
+}
+
 export const locationTypeChoices = async () => {
     const response = await fetch ("http://localhost:8088/socioLocations")
     const locations = await response.json()
+
+    document.addEventListener("change", handleLocationChange)
 
     let choicesHTML = "<h2>Which type of area do you live in?</h2>"
     for (const location of locations) {
